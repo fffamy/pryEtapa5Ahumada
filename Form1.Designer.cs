962cdbeb9244a38e492f997a79346043ab5be07f
Federico Ahumada <fedee_crc@hotmail.com>
2024-05-14 07:18:14 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace pryEtapa5Ahumada
+{
+    partial class frmEtapa5
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(281, 589);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(106, 52);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // frmEtapa5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(680, 653);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "frmEtapa5";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Etapa 5 - CREAR OBJETOS POR CÓDIGO - ALEATORIO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEtapa5Ahumada
+{
+    public partial class frmEtapa5 : Form
+    {
+        public frmEtapa5()
+        {
+            InitializeComponent();
+        }
+
+        //Creo las clases, un randomizador y un pct para las imagenes
+
+        clsVehiculo objAuto = new clsVehiculo();
+        clsVehiculo objAvion = new clsVehiculo();
+        clsVehiculo objBarco = new clsVehiculo();
+
+        Random rnd = new Random();
+
+        private PictureBox imagenAleatoria;
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            int indiceAleatorio = rnd.Next(1, 4);
+            // Elimina el pct si ya hay uno existente en pantalla
+            if (Controls.Contains(imagenAleatoria))
+            {
+                Controls.Remove(imagenAleatoria);
+                imagenAleatoria.Dispose();
+            }
+
+            switch (indiceAleatorio)
+            {
+                case 1:
+                    objAuto.crearAuto();
+                    objAuto.Auto.Location = new Point(400, 500);
+                    imagenAleatoria = objAuto.Auto;
+
+                    break;
+
+                case 2:
+                    objAvion.crearAvion();
+                    objAvion.Avion.Location = new Point(200, 100);
+                    imagenAleatoria = objAvion.Avion;
+
+                    break;
+
+                case 3:
+                    objBarco.crearBarco();
+                    objBarco.Barco.Location = new Point(300, 350);
+                    imagenAleatoria = objBarco.Barco;
+
+                    break;
+            }
+            Controls.Add(imagenAleatoria);
+
+        }
+    }
+}
